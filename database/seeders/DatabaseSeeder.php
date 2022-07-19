45c2a98623e09067560f5ee18a686ef3d80f3887
basel mohsen <105518530+baselmohsen@users.noreply.github.com>
2022-07-19 18:10:53 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             testmoSeeder::class,
             sitecontent::class,
+            SettingsSeeder::class,
 
         ]);
     }
